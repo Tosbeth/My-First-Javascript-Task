af3ad714b9d6217dabbfb48af9f371abd078606a
Tosbeth <105288291+Tosbeth@users.noreply.github.com>
2022-06-23 13:16:29 +0100

    files added via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let myName ='Name: Oluwatomisn Elizabeth Oyediran';
+let myHeight ='Height: 162cm';
+let myCountry ='Country: Nigeria';
+
+alert(myName);
+alert(myHeight);
+alert(myCountry);
+
+    © 2022 GitHub, Inc.
+
+    Terms
